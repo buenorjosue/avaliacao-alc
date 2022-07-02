9c96e8e8392b4a388ab332591c4e98a2006dc8de
josuebuenor <josuebuenor@ufrrj.br>
2022-07-01 23:54:32 -0300

    Três primeiras questões da avaliação

---
+++ questaodois.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void calculo(int m, int n, float A[m][n], float y[m], float x[n], float Ax[m]){
+    for (int i = 0; i < m; i++){
+    for (int k = 0; k < n; k++){
+        printf("Digite os valores de A: \n");
+        scanf("%f", &A[i][k]);
+    }  
+   }
+
+    for(int i = 0; i < n; i++){
+        printf("Digite os valores de x: \n");
+        scanf("%f", &x[i]);
+    }
+
+    for(int i = 0; i < m; i++){
+        printf("Digite os valores de y: \n");
+        scanf("%f", &y[i]);
+    }
+
+    for (int i = 0; i < m; i++){
+        Ax[i] = 0;
+    }
+    
+
+    for(int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            Ax[i] = A[i][j] * x[j] + Ax[i];
+        }     
+    }
+
+    for(int i = 0; i < m; i++){
+        y[i] = Ax[i] + y[i];
+    }
+
+    for(int i = 0; i < m; i++){
+        printf("O vetor y eh: %.0f\n", y[i]);
+    }
+}
+
+int main (void){
+
+   int m, n;
+
+   printf("Digite o tamanho das linhas: ");
+   scanf("%d", &m);
+
+   printf("Digite o tamanho das colunas: ");
+   scanf("%d", &n);
+
+   float A[m][n], y[m], x[n], Ax[m];
+
+   calculo(m, n, A, y, x, Ax);
+
+    return 0;
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void calculo(int tam, float a, float x[tam], float y[tam]){
+
+    printf("Digite a constante: ");
+    scanf("%f", &a);
+
+    for(int i = 0; i < tam; i++){
+        printf("Digite o elemento %d de x: ", i);
+        scanf("%f", &x[i]);
+    }
+
+    for(int i = 0; i < tam; i++){
+        printf("Digite o elemento %d de y: ", i);
+        scanf("%f", &y[i]);
+    }
+
+    for(int i = 0; i < tam; i++){
+        y[i] = (a*x[i]) + y[i];
+    }
+
+    for(int i = 0; i < tam; i++){
+        printf("Elemento %d do vetor y: %.0f\n", i, y[i]);
+    }
+}
+
+int main (void){
+
+    int tam; 
+
+    printf("Digite o tamanho dos vetores: ");
+    scanf("%d", &tam);
+
+    float a, x[tam], y[tam];
+
+    calculo(tam, a, x, y);
+
+    return 0;
+}
